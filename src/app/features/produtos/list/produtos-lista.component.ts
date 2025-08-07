be58789e1d494f1191cfb55fc167ad5c0ba566e9
FernandoSFurtado <fernando.silva.furtado@hotmail.com>
2025-08-07 18:27:11 -0300

    docs: Criando diretorios e components

---
+++ src/app/features/produtos/list/produtos-lista.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produtos-lista',
+  standalone: true,
+  imports: [],
+  templateUrl: './produtos-lista.component.html',
+  styleUrl: './produtos-lista.component.scss'
+})
+export class ProdutosListaComponent {
+
+}
